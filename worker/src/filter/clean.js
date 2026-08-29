8c6f413a270dd1e82fc82048dc04e1512da2f3d8
koolcy <viva1668@163.com>
2026-08-29 18:00:02 +0800

    Delete worker directory

--- worker/src/filter/clean.js
+++
@@ -1,5 +0,0 @@
-import { CONFIG } from "../config.js";
-
-export function isSpam(text = "") {
-  return CONFIG.BLACKLIST.some(k => text.includes(k));
-}
--- worker/src/utils/decode.js
+++
@@ -1,7 +0,0 @@
-export function decode(buffer){
-  try {
-    return new TextDecoder('utf-8',{fatal:true}).decode(buffer);
-  } catch(e){
-    return new TextDecoder('gbk').decode(buffer);
-  }
-}
